882e6be62045280635be5a6ec6ec3fd8d8a425c5
charliexiao987 <54195093+charliexiao987@users.noreply.github.com>
2019-11-06 10:40:46 +1100

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package My1stPack;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		int a = 1, b = 20, c = 0;
+		c = a+b;
+		System.out.println("Hello world, c=:"+c);
+
+	}
+
+}
